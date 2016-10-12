244647e71e1c781bda28d762372178d4ed4e3659
NedelescuVlad <vlad.nedelescu@kcl.ac.uk>
2016-10-12 13:55:16 +0000

    Colored feedback on guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,18 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
-get '/' do 
-	message = check_guess
-	erb :index, :locals => {:rand_num => SECRET_NUMBER, :message => message}
+set :port, 3000
 
+SECRET_NUMBER = rand(100)
+
+COLOR = {:OFFSET_HIGH => 25}
+
+get '/' do 
+	erb :index, :locals => {:rand_num => SECRET_NUMBER, :message => check_guess, :color => get_color}
+end
+
+def get_guess
+	guess = nil
+
+	if params[:guess] != nil
+		guess = params[:guess].to_i
+	end
+
+	return guess
 end
 
 def check_guess()
-	guess = params[:guess].to_i
+	guess = get_guess
 	message = ""
 	
-	if params[:guess] != nil 
+	if guess != nil 
 		if guess > SECRET_NUMBER 
 			message = "Guess too high!"
 		elsif guess < SECRET_NUMBER
@@ -23,4 +36,26 @@ def check_guess()
 	end
 
 	return message
+end
+
+def get_color	
+	guess = get_guess
+	color = "white"
+
+	if guess != nil
+		if guess > SECRET_NUMBER + COLOR[:OFFSET_HIGH]
+			color = "rgb(100, 0, 0)"
+		elsif guess > SECRET_NUMBER
+			color = "rgb(255, 0, 0)"
+		elsif guess < SECRET_NUMBER - COLOR[:OFFSET_HIGH]
+			color = "rgb(0, 100, 0)"
+		elsif guess < SECRET_NUMBER 
+			color = "green"
+		end
+
+	end
+
+	return color
+
+	
 end
