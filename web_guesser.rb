1726ffb18a8ae8062e89e2d9a66a291bd0977c49
NedelescuVlad <vlad.nedelescu@kcl.ac.uk>
2016-10-12 21:47:11 +0000

    Reset random number after 5 guesses. Give option to enable cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,12 +3,13 @@ require 'sinatra/reloader'
 
 set :port, 3000
 
-SECRET_NUMBER = rand(100)
+$secret_number = rand(100)
+$guesses = 4
 
 COLOR = {:OFFSET_HIGH => 25}
 
 get '/' do 
-	erb :index, :locals => {:rand_num => SECRET_NUMBER, :message => check_guess, :color => get_color}
+	erb :index, :locals => {:rand_num => $secret_number, :message => check_guess, :color => get_color}
 end
 
 def get_guess
@@ -25,17 +26,40 @@ def check_guess()
 	guess = get_guess
 	message = ""
 	
-	if guess != nil 
-		if guess > SECRET_NUMBER 
+	if guess != nil && can_guess?
+
+		$guesses -= 1
+		if guess > $secret_number 
 			message = "Guess too high!"
-		elsif guess < SECRET_NUMBER
+		elsif guess < $secret_number
 			message = "Guess too low!"
 		else
-			message = "You guessed right! The number is #{SECRET_NUMBER}"
+			message = "You guessed right! The number is #{$secret_number}"
+			message << "<br>A new secret number has been generated. Please guess again!"
+
+			reset_secret_number
 		end
+	elsif $guesses == 0
+		# No guesses left
+		message = "You are out of guesses! The number has been reset."
+
+		reset_secret_number
+	end
+
+	if params[:cheat] == 'true'
+		message << "<br>The number is #{$secret_number}"
 	end
 
 	return message
+end
+
+def reset_secret_number
+	$secret_number = rand(100)
+	$guesses = 4
+end
+
+def can_guess?
+	return $guesses > 0
 end
 
 def get_color	
@@ -43,16 +67,15 @@ def get_color
 	color = "white"
 
 	if guess != nil
-		if guess > SECRET_NUMBER + COLOR[:OFFSET_HIGH]
+		if guess > $secret_number + COLOR[:OFFSET_HIGH]
 			color = "rgb(100, 0, 0)"
-		elsif guess > SECRET_NUMBER
+		elsif guess > $secret_number
 			color = "rgb(255, 0, 0)"
-		elsif guess < SECRET_NUMBER - COLOR[:OFFSET_HIGH]
+		elsif guess < $secret_number - COLOR[:OFFSET_HIGH]
 			color = "rgb(0, 100, 0)"
-		elsif guess < SECRET_NUMBER 
+		elsif guess < $secret_number 
 			color = "green"
 		end
-
 	end
 
 	return color
